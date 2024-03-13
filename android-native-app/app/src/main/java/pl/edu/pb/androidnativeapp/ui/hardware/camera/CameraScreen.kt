d171ac13fda7d9aa34a9e000630f81d396604ded
Kacper Kalinowski <1kapka2@gmail.com>
2024-03-13 17:57:08 +0100

    android-native-app: add camera preview update compose bom to newest version

--- android-native-app/app/src/main/java/pl/edu/pb/androidnativeapp/ui/hardware/camera/CameraScreen.kt
+++ android-native-app/app/src/main/java/pl/edu/pb/androidnativeapp/ui/hardware/camera/CameraScreen.kt
@@ -1,9 +1,40 @@
 package pl.edu.pb.androidnativeapp.ui.hardware.camera
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import pl.edu.pb.androidnativeapp.ui.PermissionChecker
 
 @Composable
 fun CameraScreen() {
-    Text(text = "TODO")
+    val context = LocalContext.current
+    val cameraController = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(CameraController.IMAGE_CAPTURE)
+        }
+    }
+    PermissionChecker(
+        permission = Manifest.permission.CAMERA,
+        onDenied = { requester ->
+            LaunchedEffect(Unit) {
+                requester()
+            }
+        },
+    ) {
+        CameraPreview(controller = cameraController)
+    }
 }
