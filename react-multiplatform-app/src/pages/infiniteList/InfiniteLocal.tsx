eb90e83995bf2ebe41c22021e2bab5377c5fcfe3
akalinowski <Aleksander.kalinowski@bcplatforms.com>
2024-04-07 15:51:06 +0200

    local infinite list

--- react-multiplatform-app/src/pages/infiniteList/InfiniteLocal.tsx
+++ react-multiplatform-app/src/pages/infiniteList/InfiniteLocal.tsx
@@ -4,8 +4,7 @@ import {
   StyleSheet,
   Text,
   View,
-  Image,
-  ActivityIndicator
+  Image, ActivityIndicator,
 } from "react-native";
 import {useEffect, useState} from "react";
 import {RMCharacter, RMCharacterResult, URL} from "./types";
@@ -13,7 +12,44 @@ import api from "../../config/api";
 import {AxiosResponse} from "axios";
 
 const InfiniteListLocal = () => {
-  return <View style={styles.container}></View>
+
+  const [characters, setCharacters] = useState<RMCharacter[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    (async () => {
+      let url = URL
+      setLoading(true)
+      for(let i = 0; i < 10; i ++) {
+        const response: AxiosResponse<RMCharacterResult> = await api.get(url)
+        const characterResult: RMCharacterResult = response.data
+        url = characterResult.info.next
+        setCharacters(prevState => [...prevState, ...characterResult.results])
+      }
+      setLoading(false)
+    })()
+  }, []);
+
+  const item: ListRenderItem<RMCharacter> = ({item}) => <View style={styles.cardContainer}>
+    <View style={styles.imageContainer}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+    </View>
+    <View style={styles.infoContainer}>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.gender}>{item.gender}</Text>
+      <Text style={styles.location}>{item.status}</Text>
+      <Text style={styles.location}>{item.type}</Text>
+    </View>
+  </View>
+
+  if (loading) {
+    return <View style={{ paddingVertical: 20 }}>
+      <ActivityIndicator animating size="large" color="#007AFF" />
+    </View>
+  }
+
+  return <FlatList data={characters} renderItem={item} keyExtractor={({id}) => id.toString() + Math.random()}
+                  onEndReachedThreshold={0.5} />
 }
 
 const styles = StyleSheet.create({
@@ -22,6 +58,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '100%'
   },
+  cardContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  imageContainer: {
+    marginRight: 10,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 5,
+  },
+  infoContainer: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  gender: {
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  location: {
+    fontSize: 14,
+  },
 });
+
 
 export default InfiniteListLocal
