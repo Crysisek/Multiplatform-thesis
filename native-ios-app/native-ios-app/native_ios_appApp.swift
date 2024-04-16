a6b68543755f9aee6ce04f4e820bc2d74d34bd1d
akalinowski <Aleksander.kalinowski@bcplatforms.com>
2024-04-16 19:51:43 +0200

    Initial commit

---
+++ native-ios-app/native-ios-app/native_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_ios_appApp.swift
+//  native-ios-app
+//
+//  Created by Aleksander Kalinowski on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct native_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
