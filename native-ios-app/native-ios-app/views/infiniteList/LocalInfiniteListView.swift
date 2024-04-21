a0f8266320d0f6b2b2c1a4075310d732cf513bae
akalinowski <Aleksander.kalinowski@bcplatforms.com>
2024-04-21 13:17:24 +0200

    local infinite list

--- native-ios-app/native-ios-app/views/infiniteList/LocalInfiniteListView.swift
+++ native-ios-app/native-ios-app/views/infiniteList/LocalInfiniteListView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct LocalInfiniteListView: View {
-    var body: some View {
-        Text("Local infinite list")
-    }
+    @StateObject var characterLoader = CharacterLoader()
+    @State private var characters: [RMCharacter] = []
+    @State private var isLoading = true
+        
+        var body: some View {
+            List(characters, id: \.id) { character in
+                HStack {
+                    if let imageURL = URL(string: character.image) {
+                        AsyncImage(url: imageURL) { phase in
+                            switch phase {
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(Circle())
+                            default:
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(Circle())
+                            }
+                        }
+                    } else {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text(character.name)
+                            .font(.title)
+                        Text("Status: \(character.status.rawValue)")
+                        Text("Gender: \(character.gender.rawValue)")
+                    }
+                }
+                .padding()
+            }
+            .onAppear {
+                loadCharacters()
+            }
+        }
+        
+        func loadCharacters() {
+            isLoading = true
+            for _ in 0..<10 {
+                characterLoader.loadCharacters()
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                characters = characterLoader.characters
+                isLoading = false
+            }
+        }
 }
 
 #Preview {
