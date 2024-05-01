0b1df78468eddcf77a01d2bf75ca21d095217554
Kompanel <82094752+Kompanel@users.noreply.github.com>
2024-05-01 17:30:30 +0200

    ios-native-app: sensors (#20)
    * Initial commit
    
    * Initial commit
    
    * menu
    
    * network infinite list
    
    * local infinite list
    
    * sensors
    
    ---------
    
    Co-authored-by: akalinowski <Aleksander.kalinowski@bcplatforms.com>

--- native-ios-app/native-ios-app/views/hardware/CameraView.swift
+++ native-ios-app/native-ios-app/views/hardware/CameraView.swift
@@ -2,14 +2,71 @@
 //  CameraView.swift
 //  native-ios-app
 //
-//  Created by Aleksander Kalinowski on 21/04/2024.
+//  Created by Aleksander Kalinowski on 27/04/2024.
 //
 
 import SwiftUI
+import AVKit
+import AVFoundation
+
 
 struct CameraView: View {
+    @State private var isShowingCamera = false
+    @State private var image: UIImage?
+
     var body: some View {
-        Text("Camera")
+        VStack {
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else {
+                Text("Brak obrazu z kamery")
+            }
+            Button("Otwórz kamerę") {
+                self.isShowingCamera.toggle()
+            }
+        }
+        .sheet(isPresented: $isShowingCamera) {
+            CameraViewController(image: self.$image)
+        }
+    }
+}
+
+struct CameraViewController: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let cameraViewController = UIImagePickerController()
+        cameraViewController.delegate = context.coordinator
+        cameraViewController.sourceType = .camera
+        return cameraViewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraViewController
+
+        init(parent: CameraViewController) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let pickedImage = info[.originalImage] as? UIImage {
+                parent.image = pickedImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
 }
 
--- native-ios-app/native-ios-app/views/hardware/GpsView.swift
+++ native-ios-app/native-ios-app/views/hardware/GpsView.swift
@@ -6,11 +6,47 @@
 //
 
 import SwiftUI
+import CoreLocation
+import Foundation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation? = nil
+    
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        self.location = location
+    }
+}
 
 struct GpsView: View {
-    var body: some View {
-        Text("GPS")
-    }
+    @ObservedObject private var locationManager = LocationManager()
+
+        
+        var body: some View {
+            
+            let coordinate = self.locationManager.location != nil
+            ? self.locationManager.location!.coordinate : CLLocationCoordinate2D()
+            
+            return HStack {
+                Text("Latitude: \(coordinate.latitude)")
+                Text("Longtitude: \(coordinate.longitude)")
+            }
+        }
 }
 
 #Preview {
