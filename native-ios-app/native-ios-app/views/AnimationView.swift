73c836360c21d39d314cfacd2fc8975384f56987
Kompanel <82094752+Kompanel@users.noreply.github.com>
2024-05-03 11:09:37 +0200

    ios-native-app: animation (#21)
    * Initial commit
    
    * Initial commit
    
    * menu
    
    * network infinite list
    
    * local infinite list
    
    * sensors
    
    * animation
    
    ---------
    
    Co-authored-by: akalinowski <Aleksander.kalinowski@bcplatforms.com>

--- native-ios-app/native-ios-app/views/AnimationView.swift
+++ native-ios-app/native-ios-app/views/AnimationView.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 
 struct AnimationView: View {
+    @State private var isAnimating = false
+    
     var body: some View {
-        Text("Animation")
+        VStack {
+            Spacer()
+            AnimatedSquare()
+            Spacer()
+        }
+        .onAppear {
+            self.isAnimating = true
+        }
     }
 }
+
+struct AnimatedSquare: View {
+    @State private var yOffset: CGFloat = 0
+    @State private var color: Color = .pink
+    
+    let animationDuration: Double = 0.5
+    
+    var body: some View {
+        Rectangle()
+            .fill(color)
+            .frame(width: 100, height: 100)
+            .offset(y: yOffset)
+            .onAppear {
+                self.animate()
+            }
+    }
+    
+    func animate() {
+        withAnimation(Animation.easeInOut(duration: animationDuration).repeatForever(autoreverses: true)) {
+            self.yOffset = 250
+        }
+        
+        withAnimation(Animation.easeInOut(duration: animationDuration).repeatForever(autoreverses: true).delay(animationDuration / 2)) {
+            self.color = .red
+        }
+    }
+}
+
+
 
 #Preview {
     AnimationView()
